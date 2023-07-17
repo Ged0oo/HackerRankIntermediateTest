81ada4661f0421e9817c302adc13fd1aba91ea2c
Mohamed Nagy <mohmmednagy@gmail.com>
2023-07-17 08:34:18 +0300

    “commit”

--- 1 - Task of Pairing.c
+++ 1 - Task of Pairing.c
@@ -1,3 +1,10 @@
+/*
+*	A company sells dumbbells in pairs. These are weights for exercising. 
+*	They receive a shipment of dumbbells weighing anywhere from 1 unit up to a certain maximum. 
+*	A pair can only be sold if their weights are sufficiently close: no greater than 1 unit difference. 
+*	Given an inventory of various weights, determine the maximum number of pairs the company can sell.
+*/
+
 long taskOfPairing(int freq_count, long* freq) 
 {    
     long pairs = 0;
